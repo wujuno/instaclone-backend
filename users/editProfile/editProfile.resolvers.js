c3446ba997a8aff0f4738563267f4c3ce5ab7648
wujuno <86146780+wujuno@users.noreply.github.com>
2022-12-23 12:22:01 +0900

    Add editProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,40 @@
+import bcrypt from "bcrypt"
+import client from "../../client"
+
 export default {
     Mutation: {
-        editProfile:() =>console.log("hi")
+        editProfile: async(_,
+            {firstName,
+            lastName,
+            userName,
+            email,
+            password:newPassword}) => {
+                let uglyPassword = null;
+                if(newPassword){
+                    uglyPassword = await bcrypt.hash(newPassword,10);
+                }
+                const updatedUser = await client.user.update({
+                    where:{
+                        id:1
+                    },
+                    data:{
+                        firstName,
+                        lastName,
+                        userName,
+                        email,
+                        ...(uglyPassword && {password:uglyPassword})
+                    }
+                })
+                if(updatedUser.id){
+                    return {
+                        ok:true
+                    }
+                } else {
+                    return {
+                        ok:false,
+                        error: "Could not update profile."
+                    }
+                }
+        }
     }
 }
