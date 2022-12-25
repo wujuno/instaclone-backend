40a0a39b2601a4347903c13e0669cf7808233531
wujuno <86146780+wujuno@users.noreply.github.com>
2022-12-25 20:56:51 +0900

    cursor based pagination

---
+++ src/users/seeFollowing/seeFollowing.resolvers.ts
@@ -0,0 +1,33 @@
+import { Resolvers } from "../../types";
+
+
+const resolvers:Resolvers = {
+    Query: {
+        seeFollowing: async (_,{userName,lastId},{client})=>{
+            const ok = await client.user.findUnique({
+                where:{userName},
+                select:{id:true}
+            })
+            if(!ok){
+                return{
+                    ok: false,
+                    error:"User not found."
+                }
+            }
+            const following = await client.user
+                .findUnique({where:{userName}})
+                .following({
+                    skip: lastId ? 1 : 0,
+                    take:5,
+                    ...(lastId && {cursor:{id:lastId}})
+                });
+            return {
+                ok: true,
+                following,
+            }
+            
+        }
+    }
+}
+
+export default resolvers;
