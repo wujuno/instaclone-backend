0ff6d9217c20a2d0e756fe74c4d56bc5826b72d8
wujuno <86146780+wujuno@users.noreply.github.com>
2022-12-25 20:15:42 +0900

    offset pagination

--- src/users/seeFollowers/seeFollowers.resolvers.ts
+++ src/users/seeFollowers/seeFollowers.resolvers.ts
@@ -4,11 +4,28 @@ import { Resolvers } from "../../types";
 const resolvers:Resolvers = {
     Query: {
         seeFollowers: async (_,{userName,page},{client})=>{
-            const aFollowers = await client.user
+            const ok = await client.user.findUnique({
+                where:{userName},
+                select:{id:true}
+            })
+            if(!ok){
+                return{
+                    ok: false,
+                    error:"User not found."
+                }
+            }
+            const followers = await client.user
                 .findUnique({where:{userName}})
-                .followers();
-            const bFollowes = await client.user
-                .findMany({where:{following:{some:{userName}}}})
+                .followers({
+                    skip:(page-1)*5,
+                    take:5,
+                });
+            const totalFollowers = (await client.user.count({where:{following:{some:{userName}}}}))
+            return {
+                ok: true,
+                followers,
+                totalPages: Math.ceil(totalFollowers/5)
+            }
         }
     }
 }
